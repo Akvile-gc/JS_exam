f730b8aeed5a5fc4fcf54c6fcdd092d4dd69363a
Akvile-gc <akvile.gvildyte@gmail.com>
2022-11-28 21:23:31 +0200

    Task 8 - still in progress

---
+++ Task 8/scripts/main.js
@@ -0,0 +1,21 @@
+/* ------------------------------ TASK 8 --------------------------------------------
+Sukurkite konstruktoriaus funkciją "Calculator" (naudokite ES5), kuri gebės sukurti objektus su 4 metodais:
+sum(a, b) - priima du skaičius ir grąžina jų sumą.
+subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
+multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
+division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
+------------------------------------------------------------------------------------ */
+class Calculator{
+    sum(a, b) {
+        return a + b;
+    };
+    subtraction(a, b) {
+        return a - b;
+    };
+    multiplication(a, b) {
+        return a * b;
+    };
+    division(a, b) {
+        return a / b;
+    }
+}
